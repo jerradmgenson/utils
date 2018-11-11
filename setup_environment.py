60bacbe45b11d246847f797f64f35130c04ba72f
Jerrad <jerradgenson@gmail.com>
2018-11-11 20:01:31 +0100

    Add emacs server on boot and edit alias.

--- setup_environment.py
+++ setup_environment.py
@@ -2,30 +2,33 @@
 from argparse import ArgumentParser
 from subprocess import run
 from collections import namedtuple
-from os import environ
+from os import environ, path
+from sys import argv
 
-_PACKAGES = (('python', 'python3'),
-             ('pip', 'python3-pip'),
-             ('emacs', 'emacs' if environ.get('DESKTOP_SESSION') else 'emacs-nox'))
+from structures.struct import struct
 
-_Packages = namedtuple('Packages', [package[0] for package in _PACKAGES])
-PACKAGES = _Packages(*[package[1] for package in _PACKAGES])
+BASHRC_PATH = path.join(environ['HOME'], '.bashrc')
+BASH_PROFILE_PATH = path.join(environ['HOME'], '.bash_profile')
+PACKAGES = struct('Packages', (('python', 'python3'),
+                               ('pip', 'python3-pip'),
+                               ('emacs', 'emacs' if environ.get('DESKTOP_SESSION') or '--force-x' in argv else 'emacs-nox')))
 
 PIP_PACKAGES = 'rope', 'autopep8', 'yapf', 'black', 'flake8', 'ipython'
 
-_COMMANDS = (('create_venv', 'python3 -m venv ~/.default-venv'),
-             ('get_elpy_script', 'wget -O "install_elpy.lisp" "https://raw.githubusercontent.com/jerradmgenson/utils/emacs/install_elpy.lisp"'),
-             ('remove_emacsfile', 'rm ~/.emacs'),
-             ('install_elpy', 'emacs --script install_elpy.lisp'),
-             ('remove_elpy_script', 'rm install_elpy.lisp'),
-             ('get_emacsfile', 'wget -O "{}/.emacs" "https://raw.githubusercontent.com/jerradmgenson/utils/emacs/.emacs"'.format(environ['HOME'])))
+COMMANDS = struct('Commands', (('create_venv', 'python3 -m venv ~/.default-venv'),
+                               ('get_elpy_script', 'wget -O "install_elpy.lisp" "https://raw.githubusercontent.com/jerradmgenson/utils/emacs/install_elpy.lisp"'),
+                               ('remove_emacsfile', 'rm ~/.emacs'),
+                               ('install_elpy', 'emacs --script install_elpy.lisp'),
+                               ('remove_elpy_script', 'rm install_elpy.lisp'),
+                               ('get_emacsfile', 'wget -O "{}/.emacs" "https://raw.githubusercontent.com/jerradmgenson/utils/emacs/.emacs"'.format(environ['HOME']))))
 
-_Commands = namedtuple('Commands', [command[0] for command in _COMMANDS])
-COMMANDS = _Commands(*[command[1] for command in _COMMANDS])
-
+SUDO_COMMANDS = struct('SudoCommands', (('dnf_install', 'dnf -y install ' + ' '.join(PACKAGES)),))
 GIT_CONFIG = {'user.name': 'Jerrad Genson',
               'user.email': 'jerradgenson@gmail.com',
               'core.editor': 'emacs'}
+
+BASH_PROFILE = struct('BashProfile', (('emacs_server', ('emacs --bg-daemon="emacsserver"')),))
+BASHRC = struct('BashRC', (('emacs_client', 'alias edit="emacsclient -c -s emacsserver"'),))
 
 
 def main():
@@ -41,17 +44,37 @@ def main():
         packages = packages._replace(emacs='emacs')
 
     if not clargs.no_sudo:
-        command = 'sudo dnf -y install ' + ' '.join(packages)
-        shell(command)
+        for command in SUDO_COMMANDS:
+            shell('sudo ' + command)
 
     for command in COMMANDS:
-        shell(command)
+        if not (clargs.no_venv and command == COMMANDS.create_venv):
+            shell(command)
 
-    for pip_package in PIP_PACKAGES:
-        shell('~/.default-venv/bin/pip --no-cache-dir install ' + pip_package)
+    if not clargs.no_venv:
+        for pip_package in PIP_PACKAGES:
+            shell('~/.default-venv/bin/pip --no-cache-dir install ' + pip_package)
 
     for option, value in GIT_CONFIG.items():
         shell('git config --global {} {}'.format(option, value))
+
+    for line in BASHRC:
+        with open(BASHRC_PATH) as bashrc:
+            bashrc_text = bashrc.read()
+
+        with open(BASHRC_PATH, 'a') as bashrc:
+            if line not in bashrc_text:
+                print('Writing line to {}: {}'.format(BASHRC_PATH, line))
+                bashrc.write(line + '\n')
+
+    for line in BASH_PROFILE:
+        with open(BASH_PROFILE_PATH) as bash_profile:
+            bash_profile_text = bash_profile.read()
+
+        with open(BASH_PROFILE_PATH, 'a') as bash_profile:
+            if line not in bash_profile_text:
+                print('Writing line to {}: {}'.format(BASH_PROFILE_PATH, line))
+                bash_profile.write(line + '\n')
 
 
 def shell(command):
@@ -61,9 +84,10 @@ def shell(command):
 
 def parse_command_line():
     parser = ArgumentParser()
-    parser.add_argument('-n', '--no-sudo', action='store_true')
+    parser.add_argument('-s', '--no-sudo', action='store_true')
     parser.add_argument('-f', '--force-x', action='store_true')
     parser.add_argument('-e', '--install-elpy', action='store_true')
+    parser.add_argument('-v', '--no-venv', action='store_true')
     clargs = parser.parse_args()
     if clargs.no_sudo:
         print('Configuring environment without running sudo commands.')
@@ -72,7 +96,10 @@ def parse_command_line():
         print('Installing Emacs with X support, even in a headless environment.')
 
     if clargs.install_elpy:
-        print('Intalling Elpy only and then exiting.')
+        print('Installing Elpy only and then exiting.')
+
+    if clargs.no_venv:
+        print('Not creating a new Python virtual environment.')
 
     return clargs
 
