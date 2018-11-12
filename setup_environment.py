e6b6e9ede0e1b476addf7e727bc7064e10d4a7d9
Jerrad <jerradgenson@gmail.com>
2018-11-12 12:52:25 +0100

    Inline function `struct` in setup_environment.py.

--- setup_environment.py
+++ setup_environment.py
@@ -5,7 +5,12 @@ from collections import namedtuple
 from os import environ, path, remove
 from sys import argv
 
-from structures.struct import struct
+
+def struct(name, items):
+    struct_class = namedtuple(name, [item[0] for item in items])
+    struct_object = struct_class(*[item[1] for item in items])
+    return struct_object
+
 
 EDIT = '"emacsclient -c -s emacsserver"'
 BASHRC_PATH = path.join(environ['HOME'], '.bashrc')
--- structures/struct.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-
-def struct(name, items):
-    struct_class = namedtuple(name, [item[0] for item in items])
-    struct_object = struct_class(*[item[1] for item in items])
-    return struct_object
