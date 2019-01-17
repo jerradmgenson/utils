22b5d58128ef388275d507ed7480cc9dece7869e
jgenson <jerrad.genson@volvocars.com>
2019-01-17 12:24:14 +0100

    Add Tomorrow Night theme to emacsfile

--- setup_environment.py
+++ setup_environment.py
@@ -80,78 +80,88 @@ INSTALL_ELPY =\
     """
 
 EMACSFILE =\
-    """
-    ;; Added by Package.el.  This must come before configurations of
-    ;; installed packages.  Don't delete this line.  If you don't want it,
-    ;; just comment it out by adding a semicolon to the start of the line.
-    ;; You may delete these explanatory comments.
-    (package-initialize)
+    """;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-    (setq inhibit-splash-screen t)
-    (custom-set-variables
-     ;; custom-set-variables was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-     '(column-number-mode t)
-     '(font-use-system-font t)
-     '(global-display-line-numbers-mode t)
-     '(package-selected-packages (quote (elpy)))
-     '(show-paren-mode t))
-    (custom-set-faces
-     ;; custom-set-faces was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-     )
+(setq inhibit-splash-screen t)
 
-    (require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" default)))
+ '(font-use-system-font t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages (quote (github-elpa elpy)))
+ '(show-paren-mode t))
 
-    ;; Add Elpy repo to source packages
-    (add-to-list 'package-archives
-                 '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-    ;; Start and configure Elpy
-    (package-initialize)
-    (elpy-enable)
-    (setq elpy-rpc-python-command "python")
-    (setenv "IPY_TEST_SIMPLE_PROMPT" "1")
-    (setq python-shell-interpreter "ipython"
-          python-shell-interpreter-args "-i")
+(require 'package)
 
-    (pyvenv-activate "~/.default-venv")
-    (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
+;; Add Elpy repo to source packages
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-    ;; Remove trailing whitespace upon save
-    (setq-default indicate-empty-lines t)
-    (add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; Start and configure Elpy
+(package-initialize)
+(elpy-enable)
+(setq elpy-rpc-python-command "python")
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i")
 
-    ;; Configure Semantic
-    (setq semantic-default-submodes
-          '(;; Perform semantic actions during idle time
-            global-semantic-idle-scheduler-mode
-            ;; Use a database of parsed tags
-            global-semanticdb-minor-mode
-            ;; Highlight the name of the function you're currently in
-            global-semantic-highlight-func-mode
-            ;; show the name of the function at the top in a sticky
-            global-semantic-stickyfunc-mode
-            ;; Generate a summary of the current tag when idle
-            global-semantic-idle-summary-mode
-            ;; Show a breadcrumb of location during idle time
-            global-semantic-idle-breadcrumbs-mode
-            ;; Switch to recently changed tags with `semantic-mrub-switch-tags',
-            ;; or `C-x B'
-            global-semantic-mru-bookmark-mode))
+(pyvenv-activate "~/.default-venv")
+(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
 
-    ;; Add Semantic hooks to each language mode we want to use it with
-    (add-hook 'emacs-lisp-mode-hook 'semantic-mode)
-    (add-hook 'python-mode-hook 'semantic-mode)
-    (add-hook 'java-mode-hook 'semantic-mode)
-    (add-hook 'c-mode-hook 'semantic-mode)
-    (add-hook 'scheme-mode-hook 'semantic-mode)
+;; Remove trailing whitespace upon save
+(setq-default indicate-empty-lines t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-    """
+;; Configure Semantic
+(setq semantic-default-submodes
+      '(;; Perform semantic actions during idle time
+	global-semantic-idle-scheduler-mode
+	;; Use a database of parsed tags
+	global-semanticdb-minor-mode
+	;; Highlight the name of the function you're currently in
+	global-semantic-highlight-func-mode
+	;; show the name of the function at the top in a sticky
+	global-semantic-stickyfunc-mode
+	;; Generate a summary of the current tag when idle
+	global-semantic-idle-summary-mode
+	;; Show a breadcrumb of location during idle time
+	global-semantic-idle-breadcrumbs-mode
+	;; Switch to recently changed tags with `semantic-mrub-switch-tags',
+	;; or `C-x B'
+	global-semantic-mru-bookmark-mode))
+
+;; Add Semantic hooks to each language mode we want to use it with
+(add-hook 'emacs-lisp-mode-hook 'semantic-mode)
+(add-hook 'python-mode-hook 'semantic-mode)
+(add-hook 'java-mode-hook 'semantic-mode)
+(add-hook 'c-mode-hook 'semantic-mode)
+(add-hook 'scheme-mode-hook 'semantic-mode)
+
+;; Configure custom font and theme
+
+(load-file "~/.emacs.d/color-theme-tomorrow.el")
+(load-file "~/.emacs.d/tomorrow-night.el")
+(load-file "~/.emacs.d/column-marker.el")
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-11.0" ))
+"""
 
 FILES = [('install_elpy.lisp', INSTALL_ELPY),
          ('.emacs', EMACSFILE)]
